105f13af22a97c1758ff7f90279bf0c7224c2287
Василий <Veisner@mail.ru>
2022-12-13 15:22:49 +0300

    fix

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,12 +1,11 @@
 from django_filters import rest_framework as filters
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe
+from users.models import User
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__slug',
-        queryset=Tag.objects.all()
-    )
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
     is_favorited = filters.BooleanFilter(
         method='get_is_favorited'
     )
