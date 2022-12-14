2d6fb3ea1ac3c4fd78089dc90fbecc1ee8db0e87
Василий Лёвочкин <Veisner@mail.ru>
2022-12-14 18:47:59 +0300

    nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -143,11 +143,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/backend_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
 
